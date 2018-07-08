b2f22984617685fa7b70eef120fd6997f3971ea2
1373978075@qq.com <1373978075@qq.com>
2018-07-08 13:17:33 +0800

    init

---
+++ TB.AspNetCore.Swagger.SApi/Controllers/OrderController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TB.AspNetCore.Swagger.SApi.Controllers
+{
+    [Produces("application/json")]
+    [ApiVersion("1.0", Deprecated = false)]
+    [Route("api/v{api-version:apiVersion}/[controller]/[action]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        /// <summary>
+        /// 查询订单
+        /// </summary>
+        /// <param name="orderCode">订单号</param>
+        /// <param name="orderName">订单名称</param>
+        /// <returns></returns>
+        [HttpGet("{orderCode}/{orderName}")]
+        public OrderModel QueryOrder(string orderCode, string orderName)
+        {
+            OrderModel model = new OrderModel
+            {
+
+                OrderCode = orderCode
+
+            };
+            return model;
+        }
+
+        /// <summary>
+        /// 提交订单
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public OrderModel SubOrder([FromBody]OrderModel model)
+        {
+            return model;
+        }
+    }
+
+    public class OrderModel
+    {
+        /// <summary>
+        /// 订单号
+        /// </summary>
+        public string OrderCode { get; set; }
+
+        /// <summary>
+        /// 订单金额
+        /// </summary>
+        public decimal OrderAmount { get; set; }
+
+        /// <summary>
+        /// 订单类型
+        /// <Remark>
+        /// 0 商家入驻
+        /// 1 线下交易
+        /// </Remark>
+        /// </summary>
+        public OrderTypeInfo OrderType { get; set; }
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum OrderTypeInfo
+    {
+        /// <summary>
+        /// 商家入驻
+        /// </summary>
+        [Description("商家入驻")]
+        StoreEntry = 0,
+        /// <summary>
+        /// 线下交易
+        /// </summary>
+        [Description("线下交易")]
+        StoreTrade = 1,
+
+    }
+}
---
+++ TB.AspNetCore.Swagger.SApi/Startup.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace TB.AspNetCore.Swagger.SApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //版本控制
+            services.AddMvcCore().AddVersionedApiExplorer(o => o.GroupNameFormat = "'v'VVV");
+            services.AddApiVersioning(option =>
+            {
+                // allow a client to call you without specifying an api version
+                // since we haven't configured it otherwise, the assumed api version will be 1.0
+                option.AssumeDefaultVersionWhenUnspecified = true;
+                option.ReportApiVersions = false;
+            });
+            services.AddSwaggerGen(
+                options =>
+                {
+                    // resolve the IApiVersionDescriptionProvider service
+                    // note: that we have to build a temporary service provider here because one has not been created yet
+                    var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+
+                    // add a swagger document for each discovered API version
+                    // note: you might choose to skip or document deprecated API versions differently
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                    }
+
+                    // add a custom operation filter which sets default values
+                    options.OperationFilter<SwaggerDefaultValues>();
+
+                    //
+                    // integrate xml comments
+                    options.IncludeXmlComments(XmlCommentsFilePath);
+                    //options.IncludeXmlComments(XmlModelsFilePath);
+                });
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        /// <param name="provider">api版本描述provider</param>
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            //Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options =>
+                {
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                    }
+                });
+        }
+
+        static string XmlCommentsFilePath
+        {
+            get
+            {
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                return Path.Combine(basePath, fileName);
+            }
+        }
+
+        static string XmlModelsFilePath
+        {
+            get
+            {
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var fileName = "";//typeof(UserInfoModel).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                return Path.Combine(basePath, fileName);
+            }
+        }
+
+        static Info CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new Info()
+            {
+                Title = $"TB_AspNetCore_Swagger v{description.ApiVersion}",
+                Version = description.ApiVersion.ToString(),
+                Description = "多版本管理（点右上角版本切换）<br/>",
+                Contact = new Contact() { Name = "TopBrids_1373978075@qq.com" }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += "<br/><b>TopBrids</b>";
+            }
+
+            return info;
+        }
+    }
+}
